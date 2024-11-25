9115c37d1d32ba32602ef65f35c9bd1b5706288d
Matheus Assis <matheus.assis.ph@gmail.com>
2024-11-25 15:42:05 -0300

    first git

---
+++ codejavascript.js
@@ -0,0 +1,13 @@
+// alert ('Olá, seja bem vindo a Stillus Home');
+
+
+function slide(){
+    let count = 1;
+    document.getElementById("radio1").checked=true;
+
+    setInterval(
+        function name(params) {
+            
+        }
+    )
+}
